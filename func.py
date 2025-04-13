24a78951c1b02367e06447da472f1f276cd3e5d6
tuvshinbat t <tuvshinbat@gmail.com>
2025-04-13 22:29:11 +0800

    hm

---
+++ func.py
@@ -0,0 +1,19 @@
+#def celsius_to_fahrenheit(c):
+ ##   return ((c * 9/5) + 32)
+#a= int(input("number pls"))
+#c_to_f= celsius_to_fahrenheit(a)
+#print(f"C ees F shuljuulev:{c_to_f}")
+
+def a_b_c(a,b,x):
+    lalar= 145
+    return (a*a+b*b-x+lalar)
+
+a= int(input("a herchim pls"))
+b= int(input("b herchim pls"))
+x= int(input("x ylgavar pls"))
+c_tal= a_b_c(a,b,x)
+
+print(f"C talaas ylgavariig hasav:{c_tal}")
+
+c= a+b
+print(f"2herchimiin niilber:{c}")
